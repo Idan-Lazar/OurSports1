c499b583cb5e14ab170598cfb4feaf0f1d143567
Idan Frenkel <eidan121@gmail.com>
2018-07-22 19:59:57 +0300

    FootBallApi inside

--- OurSports1/wwwroot/js/site.js
+++ OurSports1/wwwroot/js/site.js
@@ -47,3 +47,28 @@ $(".Searchline input").click(function () {
  
    
 });
+$(function (){
+    var live;
+   
+    $.ajax({
+        url: "https://apifootball.com/api/?action=get_events&from=2017-04-01&to=2017-04-24&league_id=150&APIkey=df4ea49a08ed63529e46516ab69c3012e32ac721aafa8186a1d8c05fd61a5df6",
+        dataType: 'json', 
+        type: 'get', 
+        cache: false,
+        success: function (data) {
+            $(data).each(function (index, value) {
+                if (index < 5) {
+
+                    $(".containerjson").append("<tr class='score " + index + "'></tr>");
+                    $(".containerjson tr.score." + index).wrapInner("<table class='table'></table>").addClass("box");
+                    $(".containerjson tr.score." + index + " table").append("<tr class='score '><td class='country_name'>" + value.country_name + "</td></tr>");
+                    $(".containerjson tr.score." + index + " table").append("<tr class='score '><td class='league_name'>" + value.league_name + "</td></tr>");
+                    $(".containerjson tr.score." + index + " table").append("<tr class='score '><td class='match_date'>" + value.match_date + "</td></tr>");
+                    $(".containerjson tr.score." + index + " table").append("<tr class='score '><td class='match_hometeam_name'>" + value.match_hometeam_name +
+                        "</td>" + "<td class='match_hometeam_score'>" + value.match_hometeam_score + "</td>" + "</td>" + "<td class='match_awayteam_name'>" + value.match_awayteam_name + "</td>" + "<td class='match_awayteam_score'>" + value.match_awayteam_score + "</td>" + "</tr>");
+                    $("td.country_name").addClass("text-danger font-weight-bold");
+
+                } });
+        }
+    });
+});
